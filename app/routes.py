7dd33d4a9c79c351e696f48d5a71b9ecace10eea
VictorS67 <185000048@qq.com>
2022-06-20 10:54:59 -0400

    add template

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,140 @@ from twilio.twiml.messaging_response import MessagingResponse
 # import run_with_ngrok from flask_ngrok to run the app using ngrok
 from flask_ngrok import run_with_ngrok
 
+from datetime import datetime, timezone
+
 run_with_ngrok(app)
+
+USER = "User"
+CHATBOT = "Chatbot"
+WARNING = "warning"
+END = "end"
+NOTI = "notification"
+
+conversation = [
+    {
+        "from": CHATBOT,
+        "to": WARNING,
+        "message": "The following is a conversation with a therapist. The therapist is helpful, creative, empathetic, and very friendly.",
+        "send_time": datetime(2022, 6, 20, 0, 0, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": NOTI,
+        "message": "Yesterday",
+        "send_time": datetime(2022, 6, 20, 0, 0, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "Hello, who are you?",
+        "send_time": datetime(2022, 6, 20, 0, 0, 1, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "I am an AI created by OpenAI. How can I help you today?",
+        "send_time": datetime(2022, 6, 20, 0, 1, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "I need help.",
+        "send_time": datetime(2022, 6, 20, 0, 2, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "I'm sorry to hear that. What's going on?",
+        "send_time": datetime(2022, 6, 20, 0, 3, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": NOTI,
+        "message": "Today",
+        "send_time": datetime(2022, 6, 20, 0, 4, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "I had an argument with my family today.",
+        "send_time": datetime(2022, 6, 20, 0, 4, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "I'm sorry to hear that. Can you tell me more about what happened?",
+        "send_time": datetime(2022, 6, 20, 0, 5, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "I told my mom that I want to buy a computer but she was very upset about that.",
+        "send_time": datetime(2022, 6, 20, 0, 6, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "I'm sorry to hear that she was upset. Can you tell me more about why you want to buy a computer?",
+        "send_time": datetime(2022, 6, 20, 0, 7, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "I told her that I need a computer to complete my assignment.",
+        "send_time": datetime(2022, 6, 20, 0, 8, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "I see. And she was upset because she doesn't think you need a computer for that?",
+        "send_time": datetime(2022, 6, 20, 0, 9, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "No, she doesn't 😥.",
+        "send_time": datetime(2022, 6, 20, 0, 10, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "I'm sorry to hear that. Can you tell me more about your assignment? Maybe there's a way to do it without a computer.",
+        "send_time": datetime(2022, 6, 20, 0, 11, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": USER,
+        "to": CHATBOT,
+        "message": "There is NO WAY to finish that assignment without a computer. Why didn't she understand that??? 😡",
+        "send_time": datetime(2022, 6, 20, 0, 12, 0, tzinfo=timezone.utc)
+    }, 
+    {
+        "from": CHATBOT,
+        "to": USER,
+        "message": "It sounds like you're feeling frustrated because you feel like your mom doesn't understand your needs. Can you tell me more about that?",
+        "send_time": datetime(2022, 6, 20, 0, 13, 0, tzinfo=timezone.utc)
+    },
+    {
+        "from": CHATBOT,
+        "to": END,
+        "message": "This conversation ended by the system.",
+        "send_time": datetime(2022, 6, 20, 0, 14, 0, tzinfo=timezone.utc)
+    }
+]
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('/dialogue/index.html')
+    return render_template(
+        '/dialogue/conversation_card.html', 
+        user=USER, 
+        bot=CHATBOT, 
+        warning=WARNING, 
+        end=END,
+        notification=NOTI,
+        conversation=conversation
+    )
 
 
 @app.route('/chatsms', methods=['POST'])
