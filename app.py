08ebeb7f53af855621a5b080ea00cd41d18a6878
Langson <langson.zhang@mail.utoronto.ca>
2023-03-22 22:31:18 -0400

    fixed api key loading bug

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ import openai
 import yaml
 
 
-# def start():
-    with open('./app/static/secret.yaml') as file:
-        secret_keys = yaml.load(file, Loader=yaml.FullLoader)
-        openai.api_key = secret_keys["openai"]
-        print(f"{openai.api_key} {secret_keys["openai"]} $$$$$$$$$$$$$$$$$$ {secret_keys}")
-        openai.organization = "org-FYH4qiS0WzXH7l0pCbezhmat"
-
-#     return app
-
 
 
 if __name__ == "__main__":
--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,10 @@ app.config.update(
     SECRET_KEY = 'this-is-a-secret-key'
 )
 
+with open('./app/static/secret.yaml') as file:
+    secret_keys = yaml.load(file, Loader=yaml.FullLoader)
+    openai.api_key = secret_keys['openai']
+    openai.organization = "org-FYH4qiS0WzXH7l0pCbezhmat"
+
+
 from app import routes
