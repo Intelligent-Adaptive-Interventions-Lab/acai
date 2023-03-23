d5afa03d38924d61abb6288a13f73ac741d02b87
Langson <langson.zhang@mail.utoronto.ca>
2023-03-22 22:20:58 -0400

    fix api key loading

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from app import app
 import openai
 import yaml
 
-with open('./app/static/secret.yaml') as file:
-    secret_keys = yaml.load(file, Loader=yaml.FullLoader)
-openai.api_key = secret_keys["openai"]
-openai.organization = "org-FYH4qiS0WzXH7l0pCbezhmat"
+
+# def start():
+    with open('./app/static/secret.yaml') as file:
+        secret_keys = yaml.load(file, Loader=yaml.FullLoader)
+        openai.api_key = secret_keys["openai"]
+        print(f"{openai.api_key} {secret_keys["openai"]} $$$$$$$$$$$$$$$$$$ {secret_keys}")
+        openai.organization = "org-FYH4qiS0WzXH7l0pCbezhmat"
+
+#     return app
+
 
 
 if __name__ == "__main__":
