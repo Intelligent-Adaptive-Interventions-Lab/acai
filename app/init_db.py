310e845c229fd4bea69f0a0dbd8368ad2495b6e2
Harsh <33204114+harsh-kumar9@users.noreply.github.com>
2023-03-29 00:14:02 -0400

    Update init_db.py

--- app/init_db.py
+++ app/init_db.py
@@ -16,5 +16,9 @@ cur.execute("INSERT INTO chats (user_id, chat_log) VALUES (?, ?)",
             ('127.0.0.1', 'Content for the second chat')
             )
 
+cur.execute("INSERT INTO quiz (quiz_id, recevier, difficulty, reward, answer, actual_reward, time1, time2) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            ('127.0.0.1', '1', '1', '1', '1', '1', '1', '1')
+            )
+
 connection.commit()
 connection.close()
